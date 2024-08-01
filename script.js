e143a0cb7c5bb1cfd943013a191b8fc9a6620819
AlmasBay <108793807+AlmasBay@users.noreply.github.com>
2024-08-01 15:18:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const compliments = document.querySelectorAll('.compliments p');
+    const compliments = document.querySelectorAll('#compliments-list p');
     compliments.forEach((compliment, index) => {
         setTimeout(() => {
             compliment.style.opacity = 1;
         }, index * 1000);
     });
 });
+
+function toggleCompliments() {
+    const complimentsList = document.getElementById('compliments-list');
+    if (complimentsList.classList.contains('hidden')) {
+        complimentsList.classList.remove('hidden');
+    } else {
+        complimentsList.classList.add('hidden');
+    }
+}
+
